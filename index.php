6cc7f40540500bda3072e45de5000b277306d0c1
Oeung Chheanghok <chheanghok_o@kubotacambodia.com>
2025-01-10 13:02:36 +0700

    title index.php

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <script src="https://cdn.tailwindcss.com"></script>
+        <title> Loy Team Project </title>
     </head>
 
     <body class="bg-gray-100 white:bg-gray-900">
