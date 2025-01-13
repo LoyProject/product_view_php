defcfe794bfe6e3571eace411f716b52350c7119
moun_sovongdy <sovongdy.m@gmail.com>
2025-01-13 11:52:41 +0700

    test db-connection.php

--- database/fetch_product.php
+++ database/fetch_product.php
@@ -1,14 +1,7 @@
 <?php
-    $servername = "220.158.232.172";
-    $username = "product_mh01";
-    $password = "cL6sC3iRnWc3APyK";
-    $dbname = "product_mh01";
+    header('Content-Type: application/json');
 
-    $conn = new mysqli($servername, $username, $password, $dbname);
-
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
+    include 'db_connection.php';
 
     $sql = "SELECT * FROM products";
 
--- database/insert_product.php
+++ database/insert_product.php
@@ -1,14 +1,6 @@
 <?php
-    $servername = "220.158.232.172";
-    $username = "product_mh01";
-    $password = "cL6sC3iRnWc3APyK";
-    $dbname = "product_mh01";
-
-    $conn = new mysqli($servername, $username, $password, $dbname);
-
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
+    
+    include 'db_connection.php';
 
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $name = $_POST['name'];
