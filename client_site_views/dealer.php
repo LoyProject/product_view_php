f4c869c4cf04aa0e162761ca87546ad1ac07a24d
LoyProject <loyproject.team@gmail.com>
2025-01-25 17:03:46 +0700

    dealer UI

--- client_site_views/dealer.php
+++ client_site_views/dealer.php
@@ -20,53 +20,25 @@
                         see our products and services. You can also get a consultation from our team.
                     </p>
                 </div>
-
                 <div class="grid lg:grid-cols-4 md:grid-cols-3 gap-6 max-md:justify-center mt-8">
                     <div class="border rounded-lg overflow-hidden">
                         <img src="https://readymadeui.com/team-1.webp" class="w-full h-56 object-cover" />
                         <div class="p-4">
                             <h4 class="text-gray-800 text-base font-bold">John Doe</h4>
+                            <div class="space-x-2 flex items-center">
+                                <p class="text-gray-500 text-xs mt-1 leading-relaxed hover:text-red-500">+85511425717</p>
+                            </div>
                             <article class="text-wrap">
-                                <p class="text-gray-800 text-xs mt-1 leading-relaxed break-words">Software Engineer
-                                    sdfsdfsdfssdfsdfsdfsdfsdfwefwfwefwesdssfsdfsfsfds
+                                <p class="text-gray-500 text-xs mt-1 leading-relaxed break-words hover:text-red-500"> 22D1E0, St01, Dangkor, Dangkor, Phnom Penh, Cambodia</p>
                                 </p>
                             </article>
-                            <div class="space-x-4 mt-4">
-                                <button type="button"
-                                    class="w-6 h-6 inline-flex items-center justify-center rounded-full border-none outline-none bg-[#03a9f4] hover:bg-[#03a1f4]">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="14px" fill="#fff"
-                                        viewBox="0 0 512 512">
-                                        <path
-                                            d="M508.5 71.5L17.5 221.5c-13.2 4.2-12.9 13.6-2.2 17.1l117.4 36.8 45.1 140.4c5.5 15.2 11 20.8 22.3 20.8 9.2 0 13.3-4.2 22.9-12.9l53.7-51.7 111.4 82.7c20.5 11.4 35 5.5 40.1-18.7l72.7-348.6c6.7-28.5-10.7-41.3-29.5-34.1zM186.6 282.7L380.7 145c10.4-6.5 19.8-2.9 12.1 4.1L215.2 320.7l-8.5 59.6-20.1-97.6z"
-                                            data-original="#03a9f4" />
-                                    </svg>
-                                </button>
-                                <button type="button"
-                                    class="w-6 h-6 inline-flex items-center justify-center rounded-full border-none outline-none bg-blue-600 hover:bg-blue-700">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="14px" fill="#fff"
-                                        viewBox="0 0 155.139 155.139">
-                                        <path
-                                            d="M89.584 155.139V84.378h23.742l3.562-27.585H89.584V39.184c0-7.984 2.208-13.425 13.67-13.425l14.595-.006V1.08C115.325.752 106.661 0 96.577 0 75.52 0 61.104 12.853 61.104 36.452v20.341H37.29v27.585h23.814v70.761h28.48z"
-                                            data-original="#010002" />
-                                    </svg>
-                                </button>
-                                <button type="button"
-                                    class="w-6 h-6 inline-flex items-center justify-center rounded-full border-none outline-none bg-[#0077b5] hover:bg-[#0055b5]">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="14px" fill="#fff"
-                                        viewBox="0 0 24 24">
-                                        <path
-                                            d="M12 0h4.45a6.52 6.52 0 0 0 2.75 5.72A6.34 6.34 0 0 0 23.99 8v4.46h-2.89a9.22 9.22 0 0 1-5.18-1.59v6.88a5.88 5.88 0 1 1-5.88-5.88c.23 0 .45.01.67.03v3.1a2.77 2.77 0 1 0 2.77 2.77V0z"
-                                            data-original="#0077b5" />
-                                    </svg>
-                                </button>
-                            </div>
                             <div class="mt-4">
                                 <a href="https://maps.app.goo.gl/6iHi9N8s6vtxb3yy7" target="_blank">
                                     <button
-                                        class="w-full bg-[#03a9f4] hover:bg-[#03a1f4] text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline font-[sans-serif]">
-                                        Visit
+                                        class="w-full bg-red-500 hover:bg-red-700 text-white font-xs py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+                                        View Location
                                     </button>
-                                </a>
+                                </a> 
                             </div>
                         </div>
                     </div>
