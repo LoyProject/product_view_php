1830aee0837d34ae3f40eb5960c5bd89757dbc75
LoyProject <loyproject.team@gmail.com>
2025-01-26 17:30:36 +0700

    read data dealer page

--- client_site_views/dealer.php
+++ client_site_views/dealer.php
@@ -8,10 +8,11 @@
     <title> Loy Team Project </title>
 </head>
 
-<?php include 'header.php'?>
+<?php include 'header.php' ?>
 
 <body>
-    <div class="container mx-auto p-16 mt-8">
+    
+    <div class="px-8 mb-8 mt-24 container-md mx-auto">
         <div class="font-sans">
             <div class="lg:max-w-5xl max-w-3xl mx-auto">
                 <div class="max-w-2xl mx-auto text-center">
@@ -20,33 +21,56 @@
                         see our products and services. You can also get a consultation from our team.
                     </p>
                 </div>
-                <div class="grid lg:grid-cols-4 md:grid-cols-3 gap-6 max-md:justify-center mt-8">
-                    <div class="border rounded-lg overflow-hidden">
-                        <img src="https://readymadeui.com/team-1.webp" class="w-full h-56 object-cover" />
-                        <div class="p-4">
-                            <h4 class="text-gray-800 text-base font-bold">John Doe</h4>
-                            <div class="space-x-2 flex items-center">
-                                <p class="text-gray-500 text-xs mt-1 leading-relaxed hover:text-red-500">+85511425717</p>
-                            </div>
-                            <article class="text-wrap">
-                                <p class="text-gray-500 text-xs mt-1 leading-relaxed break-words hover:text-red-500"> 22D1E0, St01, Dangkor, Dangkor, Phnom Penh, Cambodia</p>
-                                </p>
-                            </article>
-                            <div class="mt-4">
-                                <a href="https://maps.app.goo.gl/6iHi9N8s6vtxb3yy7" target="_blank">
-                                    <button
-                                        class="w-full bg-red-500 hover:bg-red-700 text-white font-xs py-2 px-4 rounded focus:outline-none focus:shadow-outline">
-                                        View Location
-                                    </button>
-                                </a> 
-                            </div>
-                        </div>
+                <div id="dealer-grid" class="mt-8 grid grid-cols-1 sm:grid-cols-1 lg:grid-cols-5 gap-2 w-full">
+                    <?php
+
+                    include '../database/db_connection.php';
+
+                    $sql = "SELECT name, contact, address, map, image FROM dealers LIMIT 5";
+                    $result = $conn->query($sql);
+
+                    if ($result->num_rows > 0) {
+                        while ($row = $result->fetch_assoc()) {
+                            echo '<div class="border rounded-lg overflow-hidden">';
+                                echo '<img src="../images_dealer/' . $row["image"] . '" class="w-full h-56 object-cover" />';
+                                echo '<div class="p-4">';
+                                    echo '<h4 class="text-gray-800 text-base font-bold">' . $row["name"] . '</h4>';
+                                    echo '<div class="space-x-2 flex items-center">';
+                                        echo '<p class="text-gray-500 text-xs mt-1 leading-relaxed hover:text-red-500">' . $row["contact"] . '</p>';
+                                    echo '</div>';
+                                    echo '<article class="text-wrap">';
+                                        echo '<p class="text-gray-500 text-xs mt-1 leading-relaxed break-words hover:text-red-500">' . $row["address"] . '</p>';
+                                    echo '</article>';
+                                    echo '<div class="mt-4">';
+                                        echo '<a href="' . $row["map"] . '" target="_blank">';
+                                            echo '<button class="w-full bg-red-500 hover:bg-red-700 text-white font-xs py-2 px-4 rounded focus:outline-none focus:shadow-outline">';
+                                                echo 'View Location';
+                                            echo '</button>';
+                                        echo '</a>';
+                                    echo '</div>';
+                                echo '</div>';
+                            echo '</div>';
+                        }
+                    } else {
+                        echo "0 results";
+                    }
+                    $conn->close();
+                    ?>
+                </div>
+                <div class="mt-8">
+                <div class="container mx-auto">
+                    <div class="flex justify-center">
+                        <button id="load-more" type="button"
+                            class="px-5 py-2.5 w-52 rounded-lg text-red-300 text-sm tracking-wider font-semibold border-2 border-none outline-none hover:border-red-500 hover:text-red-500">
+                            Load More
+                        </button>
                     </div>
                 </div>
             </div>
+            </div>
         </div>
     </div>
-    <?php include 'footer.php'?>
+    <?php include 'footer.php' ?>
 </body>
 
 </html>
--- client_site_views/footer.php
+++ client_site_views/footer.php
@@ -1,4 +1,4 @@
-<footer class="font-sans tracking-wide bg-gray-300 text-black px-10 pt-12 pb-6">
+<footer class="font-sans tracking-wide bg-gray-200 text-black px-10 pt-12 pb-6">
     <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
         <div class="sm:col-span-2 max-w-sm">
             <div class="mb-8">
@@ -7,7 +7,7 @@
               </a>
             </div>
             <h4 class="text-base font-semibold mb-6 text-red-500">About Us</h4>
-            <p class="text-white text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean gravida, mi
+            <p class="text-gray-500 text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean gravida, mi
                 eu
                 pulvinar cursus, sem elit interdum mauris.</p>
         </div>
@@ -15,24 +15,24 @@
         <div>
             <h4 class="text-base font-semibold mb-6 text-red-500">Our Page</h4>
             <ul class="space-y-3">
-                <li><a href="#" class="text-white hover:text-red-500 text-sm">Home</a></li>
-                <li><a href="#" class="text-white hover:text-red-500 text-sm">Dealer</a></li>
-                <li><a href="#" class="text-white hover:text-red-500 text-sm">Services</a></li>
+                <li><a href="#" class="text-gray-500 hover:text-red-500 text-sm">Home</a></li>
+                <li><a href="#" class="text-gray-500 hover:text-red-500 text-sm">Dealer</a></li>
+                <li><a href="#" class="text-gray-500 hover:text-red-500 text-sm">Services</a></li>
             </ul>
         </div>
 
         <div class="space-y-3">
             <h4 class="text-base font-semibold mb-6 text-red-500">Contact Us</h4>
-            <p class="text-white text-sm">123 Main Street</p>
-            <p class="text-white text-sm">City, State, Country</p>
-            <p class="text-white text-sm">contact@example.com</p>
-            <p class="text-white text-sm">+1 234 567 890</p>
+            <p class="text-gray-500 text-sm">123 Main Street</p>
+            <p class="text-gray-500 text-sm">City, State, Country</p>
+            <p class="text-gray-500 text-sm">contact@example.com</p>
+            <p class="text-gray-500 text-sm">+1 234 567 890</p>
         </div>
     </div>
 
-    <hr class=" mt-8 border-gray-200" />
+    <hr class=" mt-8 border-gray-500" />
 
     <div class="mt-4">
-        <p class='text-gray-400 text-sm'>© ReadymadeUI. All rights reserved.</p>
+        <p class='text-gray-500 text-sm'>© ReadymadeUI. All rights reserved.</p>
     </div>
 </footer>
